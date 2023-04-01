72bcd0568c618568c0b483ca51fc4ca9a1da71f4
amida zulfa <100895165+amizulfa@users.noreply.github.com>
2023-04-01 20:21:44 +0700

    Update sjfNonPreemptive.py

--- sjfNonPreemptive.py
+++ sjfNonPreemptive.py
@@ -6,7 +6,8 @@ class sjfNonPreemptive:
         self.total_turnaround_time = 0
         self.current_time = 0
         self.completed_processes = []
-
+        self.gantt_chart = []
+        
     def run(self):
         while len(self.completed_processes) < self.n:
             shortest_process = None
@@ -22,8 +23,9 @@ class sjfNonPreemptive:
                 self.total_waiting_time += shortest_process['waiting_time']
                 self.total_turnaround_time += shortest_process['turnaround_time']
                 self.completed_processes.append(shortest_process)
+                self.gantt_chart.append((shortest_process['id'], self.current_time, self.current_time + shortest_process['burst_time']))
                 self.current_time += shortest_process['burst_time']
-
+                
         avg_waiting_time = self.total_waiting_time / self.n
         avg_turnaround_time = self.total_turnaround_time / self.n
 
@@ -35,6 +37,20 @@ class sjfNonPreemptive:
         print("Average waiting time:", avg_waiting_time)
         print("Average turnaround time:", avg_turnaround_time)
 
-
+        # Print the gantt chart
+        print("\nGantt Chart:")
+        print("--------------------------------------------------")
+        prev_id = None
+        for i in range(len(self.gantt_chart)):
+            if i == 0:
+                print("|  P{}  |  {}  |  {}  |".format(self.gantt_chart[i][0], self.gantt_chart[i][1], self.gantt_chart[i][2]))
+                prev_id = self.gantt_chart[i][0]
+            else:
+                if prev_id == self.gantt_chart[i][0]:
+                    print("|      |  {}  |  {}  |".format(self.gantt_chart[i][1], self.gantt_chart[i][2]))
+                else:
+                    print("|  P{}  |  {}  |  {}  |".format(self.gantt_chart[i][0], self.gantt_chart[i][1], self.gantt_chart[i][2]))
+                    prev_id = self.gantt_chart[i][0]
+        print("\n--------------------------------------------------")
 
 

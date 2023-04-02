9c9d957df76d39d2ddb9851c132ce3f2e5f8e243
amida zulfa <100895165+amizulfa@users.noreply.github.com>
2023-04-02 16:05:04 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 from FCFS import FCFS
 from sjfNonPreemptive import sjfNonPreemptive
-from sjfPreemptive import Process
-from sjfPreemptive import shortest_remaining_time_first
+from sjfPreemptive import ShortestJobFirst
+
+
 fcfs = FCFS()
 
 while True:
@@ -21,28 +22,27 @@ while True:
             n = int(input("Enter number of processes: "))
             processes = []
             for i in range(n):
-                arrival_time = int(input("Enter arrival time of process {}: ".format(i+1)))
-                burst_time = int(input("Enter burst time of process {}: ".format(i+1)))
-                process = {'id': i+1, 'arrival_time': arrival_time, 'burst_time': burst_time, 'waiting_time': None, 'turnaround_time': None}
+                arrival_time = int(
+                    input("Enter arrival time of process {}: ".format(i+1)))
+                burst_time = int(
+                    input("Enter burst time of process {}: ".format(i+1)))
+                process = {'id': i+1, 'arrival_time': arrival_time,
+                           'burst_time': burst_time, 'waiting_time': None, 'turnaround_time': None}
                 processes.append(process)
 
             sjf = sjfNonPreemptive(processes)
             sjf.run()
-    elif option ==3:
-        if __name__ == '__main__':
-            num_processes = int(input("Enter number of processes: "))
+    elif option == 3:
+        if __name__ == "__main__":
+            p = int(input("Enter number of processes: "))
             processes = []
-            for i in range(num_processes):
-                pid = f"P{i+1}"
-                arrival_time = int(input(f"Enter arrival time for process {pid}: "))
-                burst_time = int(input(f"Enter burst time for process {pid}: "))
-                processes.append(Process(pid, arrival_time, burst_time))
+            for i in range(p):
+                process_id = input(f"Masukkan ID proses {i+1}: ")
+                arrival_time = int(input(f"Masukkan waktu kedatangan proses {i+1}: "))
+                burst_time = int(input(f"Masukkan waktu burst proses {i+1}: "))
+                processes.append([process_id, arrival_time, burst_time, 0, 0, 0, burst_time])
 
-            completed_processes, avg_waiting_time, avg_turnaround_time = shortest_remaining_time_first(processes)
+            sjf = ShortestJobFirst(processes)
+            sjf.shortest_job_first()
+            sjf.display_gantt_chart()
 
-            print("Process ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time")
-            for p in completed_processes:
-                print(f"{p.pid}\t\t{p.arrival_time}\t\t{p.burst_time}\t\t{p.waiting_time}\t\t{p.turnaround_time}")
-
-            print(f"\nAverage waiting time: {avg_waiting_time}")
-            print(f"Average turnaround time: {avg_turnaround_time}")
